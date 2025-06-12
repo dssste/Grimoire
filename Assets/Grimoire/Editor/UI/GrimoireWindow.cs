bb24bde7cc29e02976840a5224f3b34757862b2a
cz <cyz36@pm.me>
2025-06-13 00:16:13 +0800

    poc the table

--- Assets/Grimoire/Editor/UI/GrimoireWindow.cs
+++ Assets/Grimoire/Editor/UI/GrimoireWindow.cs
@@ -22,26 +22,43 @@ namespace Grimoire.Inspector {
 			if (vta == null) return;
 
 			var queryField = new TextField();
-			queryField.SetValueWithoutNotify("t:");
 			rootVisualElement.Add(queryField);
+
 			var v0 = new VisualElement();
 			v0.style.flexDirection = FlexDirection.Row;
 			rootVisualElement.Add(v0);
+
+			var v1 = new VisualElement();
+			v0.Add(v1);
+
+			var v2 = new VisualElement();
+			v2.style.flexDirection = FlexDirection.Row;
+			v0.Add(v2);
 			queryField.RegisterCallback<ChangeEvent<string>>(ev => {
-				v0.Clear();
-				string[] guids = AssetDatabase.FindAssets(ev.newValue);
-				foreach (string guid in guids) {
+				v1.Clear();
+				v2.Clear();
+				foreach (var guid in AssetDatabase.FindAssets(ev.newValue)) {
 					var asset = AssetDatabase.LoadAssetAtPath<Object>(AssetDatabase.GUIDToAssetPath(guid));
 
 					if (asset != null) {
 						var ve = new VisualElement();
 						var so = new SerializedObject(asset);
-						ve.Bind(so);
-						ve.Add(AlchemyEditor.CreateEditor(so.targetObject).CreateInspectorGUI());
-						v0.Add(ve);
+						ve.Add(new Label(asset.name));
+						var inspector = AlchemyEditor.CreateEditor(so.targetObject).CreateInspectorGUI();
+						inspector.Bind(so);
+						inspector.RegisterCallbackOnce<GeometryChangedEvent>(ev => {
+							inspector.Query<Label>(className: "unity-property-field__label").ForEach(ve => {
+								ve.RemoveFromHierarchy();
+								v1.Add(ve);
+							});
+						});
+						ve.Add(inspector);
+						v2.Add(ve);
 					}
 				}
 			});
+
+			queryField.value = "t:Monster";
 		}
 	}
 }
